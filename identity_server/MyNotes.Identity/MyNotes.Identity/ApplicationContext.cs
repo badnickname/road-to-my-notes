5219890cfc6c3b556f73bb975e8f33e871cb31bd
Bogdan Malcev <bogdan-malcev@yandex.ru>
2023-03-06 01:22:23 +0700

    identity init

---
+++ identity_server/MyNotes.Identity/MyNotes.Identity/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyNotes.Identity;
+
+public sealed class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
+}
--- identity_server/MyNotes.Identity/MyNotes.Identity/Program.cs
+++ identity_server/MyNotes.Identity/MyNotes.Identity/Program.cs
@@ -1,6 +1,49 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using MyNotes.Identity;
+using NLog.Web;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseNLog();
+builder.Configuration.AddJsonFile("appsettings.Development.json");
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
+    options.UseOpenIddict();
+});
+builder.Services.AddControllers();
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+            .UseDbContext<ApplicationContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetTokenEndpointUris("connect/token");
+        options.AllowClientCredentialsFlow();
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+        options.UseAspNetCore()
+            .EnableTokenEndpointPassthrough();
+    })
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+        options.UseAspNetCore();
+    });
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(options =>
+{
+    options.MapControllers();
+    options.MapDefaultControllerRoute();
+});
 
 app.Run();
