478cbb1f96db444ec0b3cffbe53cba3bf0239d19
Bogdan Malcev <bogdan-malcev@yandex.ru>
2023-03-20 00:13:54 +0700

    авторизация в приложении

--- identity_server/MyNotes.Identity/MyNotes.Identity/Controllers/AuthorizeController.cs
+++ identity_server/MyNotes.Identity/MyNotes.Identity/Controllers/AuthorizeController.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections.Immutable;
 using System.Security.Claims;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using MyNotes.Identity.Models;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
+using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace MyNotes.Identity.Controllers;
 
 public sealed class AuthorizeController : Controller
 {
+    private readonly IOpenIddictApplicationManager _application;
+    private readonly IOpenIddictAuthorizationManager _authorization;
     private readonly EmailService _emailService;
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
 
     public AuthorizeController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager,
-        EmailService emailService)
+        EmailService emailService, IOpenIddictApplicationManager application,
+        IOpenIddictAuthorizationManager authorization)
     {
         _signInManager = signInManager;
         _userManager = userManager;
         _emailService = emailService;
+        _application = application;
+        _authorization = authorization;
     }
 
     /// <summary>
@@ -34,7 +41,7 @@ public sealed class AuthorizeController : Controller
     }
 
     /// <summary>
-    ///     Аутентифицироваться как пользователь
+    ///     Аутентифицироваться в приложении как пользователь
     /// </summary>
     /// <param name="userName">Имя</param>
     /// <param name="password">Пароль</param>
@@ -43,7 +50,18 @@ public sealed class AuthorizeController : Controller
     public async Task<IActionResult> AuthorizeUser(string userName, string password)
     {
         var user = await _userManager.FindByNameAsync(userName);
+
         if (user is null || !user.EmailConfirmed) return View("Index", new AuthorizeResult { HasError = true });
+
+        var request = HttpContext.GetOpenIddictServerRequest()!;
+
+        var application = await _application.FindByIdAsync(request.ClientId);
+
+        var clientId = await _application.GetIdAsync(application);
+
+        object? authorization = null;
+        await foreach (var auth in _authorization.FindAsync(user.Id, clientId, Statuses.Valid,
+                           AuthorizationTypes.Permanent)) authorization = auth;
 
         var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
         if (!result.Succeeded) return View("Index", new AuthorizeResult { HasError = true });
@@ -51,20 +69,25 @@ public sealed class AuthorizeController : Controller
         var roles = (await _userManager.GetRolesAsync(user)).ToImmutableArray();
 
         var identity =
-            new ClaimsIdentity(TokenValidationParameters.DefaultAuthenticationType, OpenIddictConstants.Claims.Name,
-                OpenIddictConstants.Claims.Role);
+            new ClaimsIdentity(TokenValidationParameters.DefaultAuthenticationType, Claims.Name,
+                Claims.Role);
         identity
-            .SetClaim(OpenIddictConstants.Claims.Subject, user.Id)
-            .SetClaim(OpenIddictConstants.Claims.Email, user.Email)
-            .SetClaim(OpenIddictConstants.Claims.Name, user.UserName)
-            .SetClaims(OpenIddictConstants.Claims.Role, roles);
+            .SetClaim(Claims.Subject, user.Id)
+            .SetClaim(Claims.Email, user.Email)
+            .SetClaim(Claims.Name, user.UserName)
+            .SetClaims(Claims.Role, roles);
+
+        authorization ??= await _authorization.CreateAsync(identity, user.Id, clientId, AuthorizationTypes.Permanent,
+            identity.GetScopes());
+
+        identity.SetAuthorizationId(await _authorization.GetIdAsync(authorization));
 
         identity.SetDestinations(static claim => claim.Type switch
         {
-            OpenIddictConstants.Claims.Name or OpenIddictConstants.Claims.Subject
-                or OpenIddictConstants.Claims.Role => new[]
-                    { OpenIddictConstants.Destinations.AccessToken, OpenIddictConstants.Destinations.IdentityToken },
-            _ => new[] { OpenIddictConstants.Destinations.AccessToken }
+            Claims.Name or Claims.Subject
+                or Claims.Role => new[]
+                    { Destinations.AccessToken, Destinations.IdentityToken },
+            _ => new[] { Destinations.AccessToken }
         });
 
         return SignIn(new ClaimsPrincipal(identity), OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
