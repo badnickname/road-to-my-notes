d65129297c6c2c0adc5cb7982c003ced20e25fcc
Bogdan Malcev <bogdan-malcev@yandex.ru>
2023-03-27 13:20:55 +0700

    auth service

--- application_client/src/app/services/auth.service.ts
+++ application_client/src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { User, UserManager } from "oidc-client-ts";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly userManager = new UserManager({
+    authority: environment.identityServer.url,
+    client_id: environment.identityClient.clientId,
+    redirect_uri: `${environment.identityClient.redirectUrl}signin-callback`,
+    post_logout_redirect_uri: environment.identityClient.redirectUrl,
+    response_type: 'code',
+  })
 
-  constructor() { }
+  public async getUser(): Promise<User> {
+    const user = await this.userManager.getUser();
+    if (!user) throw new Error('user is empty');
+    return user;
+  }
+
+  public signInRedirect(): Promise<void> {
+    return this.userManager.signinRedirect();
+  }
+
+  public async renewToken(): Promise<User> {
+    const user = await this.userManager.signinSilent();
+    if (!user) throw new Error('renew user is empty');
+    return user;
+  }
 }
