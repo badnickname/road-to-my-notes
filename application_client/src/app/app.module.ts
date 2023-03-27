9ec801743f57dd86d1f35769693c1c66652275f5
Bogdan Malcev <bogdan-malcev@yandex.ru>
2023-03-27 12:54:36 +0700

    upd frontend

--- application_client/src/app/app.module.ts
+++ application_client/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ application_client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  identityClient: {
+    clientId: 'my-notes-application-client',
+    redirectUrl: 'https://application_client',
+  },
+  identityServer: {
+    url: 'https://identity_server'
+  },
+  applicationServer: {
+    url: 'https://application_server'
+  }
+}
